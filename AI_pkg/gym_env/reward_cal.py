e2dbcdf2b36b4379d63f2047f739443834c14509
chenfukye <kye1234321@gmail.com>
2024-12-16 17:31:26 +0800

    [UPDATE] Update reward function to make model better

--- AI_pkg/gym_env/reward_cal.py
+++ AI_pkg/gym_env/reward_cal.py
@@ -1,8 +1,8 @@
 """
 This module is used to calculate the reward based on the action and observation
 """
-
-def reward_cal(data: dict, prev_data: dict, step: int):
+import copy
+def reward_cal(data_list: dict, prev_data_list: dict, step: int):
     """
     Calculate the reward based on the action and observation
 
@@ -17,18 +17,37 @@ def reward_cal(data: dict, prev_data: dict, step: int):
     # Calculate the reward
     reward = 0
 
-    # print("data: ", data)
-    # print("prev_data: ", prev_data)
+    data = copy.deepcopy(data_list)
+    prev_data = copy.deepcopy(prev_data_list)
 
-    # print("decay_distance_to_goal: ", decay_distance_to_goal)
-    # distance_to_stright_line = data["distance_to_stright_line"]
-
-
-
+    # Calculate the reward based on the angle_x and angle_z
+    angle_x = data["spot_angle"][0] if data["spot_angle"][0] < 180 else 360 - data["spot_angle"][0]
     angle_z = data["spot_angle"][2] if data["spot_angle"][2] < 180 else 360 - data["spot_angle"][2]
 
-    reward =  - 1 * pow(angle_z, 2) + 1 * step
+    if prev_data["spot_angle"][0] < 180:
+        angle_x_prev = prev_data["spot_angle"][0]
+    else:
+        angle_x_prev = 360 - prev_data["spot_angle"][0]
+
+    if prev_data["spot_angle"][2] < 180:
+        angle_z_prev = prev_data["spot_angle"][2]
+    else:
+        angle_z_prev = 360 - prev_data["spot_angle"][2]
+
+    angle_x_diff = abs(angle_x) - abs(angle_x_prev)
+    angle_z_diff = abs(angle_z) - abs(angle_z_prev)
+
+    cost = angle_x_diff + angle_z_diff
+    if cost > 50:
+        reward -= 50
+    else:
+        reward -= cost
+
+    reward += 2 * step
 
     if angle_z < 3:
-        reward += 10
+        reward += 5
+    if angle_x < 3:
+        reward += 5
+
     return reward
