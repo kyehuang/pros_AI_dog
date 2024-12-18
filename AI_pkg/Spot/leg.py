afa707a6e5518b8a1e3e347062b55f24bc9c3350
chenfukye <kye1234321@gmail.com>
2024-12-18 14:18:22 +0800

    [UPDATE] leg.py

--- AI_pkg/Spot/leg.py
+++ AI_pkg/Spot/leg.py
@@ -18,36 +18,43 @@ class Leg():
         self.angles = angles
 
 
-def calculate_ik(leg:Leg, target_position:list, base_postion:list):
-    """
-    This method is used to calculate leg position
-    """
-    x = target_position[0] - base_postion[0]
-    y = target_position[1] - base_postion[1]
-    z = target_position[2] - base_postion[2]
+    def calculate_ik(self, target_position:list, base_postion:list):
+        """
+        This method is used to calculate leg position
 
-    # print(x, y, z)
-    ### calaculate first joint 1 angle
-    c = np.sqrt(z * z + y * y)
-    d = np.sqrt(c * c - leg.joint_lengths[0] * leg.joint_lengths[0])
-    joint_1_angle = np.arctan(d/leg.joint_lengths[0]) + np.arctan(y/-z)
-    # print(np.degrees(joint_1_angle))
+        Parameters:
+            target_position (list): the target position of the leg
+            base_postion (list): the base position of the leg
 
-    ### calaculate first joint 3 angle
-    g = np.sqrt(x * x + d * d)
-    numerator = g * g - leg.joint_lengths[1] * leg.joint_lengths[1] - leg.joint_lengths[2] * leg.joint_lengths[2]
-    denominator = 2 * leg.joint_lengths[1] * leg.joint_lengths[2]
-    joint_3_angle = np.arccos(numerator/denominator)
-    # print(np.degrees(joint_3_angle))
+        Returns:
+            dict: the dictionary of joint angles
+        """
+        x = target_position[0] - base_postion[0]
+        y = target_position[1] - base_postion[1]
+        z = target_position[2] - base_postion[2]
 
-    ### calaculate first joint 2 angle
+        # print(x, y, z)
+        ### calaculate first joint 1 angle
+        c = np.sqrt(z * z + y * y)
+        d = np.sqrt(c * c - self.joint_lengths[0] * self.joint_lengths[0])
+        joint_1_angle = np.arctan(d/self.joint_lengths[0]) + np.arctan(y/-z)
+        # print(np.degrees(joint_1_angle))
 
-    joint_2_angle = np.arctan(x/d) + np.arcsin(leg.joint_lengths[2] * np.sin(joint_3_angle)/g)
-    # print(np.degrees(np.arcsin(leg.joint_lengths[2] * np.sin(joint_3_angle)/g)))
+        ### calaculate first joint 3 angle
+        g = np.sqrt(x * x + d * d)
+        numerator = g * g - self.joint_lengths[1] * self.joint_lengths[1] - self.joint_lengths[2] * self.joint_lengths[2]
+        denominator = 2 * self.joint_lengths[1] * self.joint_lengths[2]
+        joint_3_angle = np.arccos(numerator/denominator)
+        # print(np.degrees(joint_3_angle))
 
-    return {"joint_1_angle": np.degrees(joint_1_angle),
-            "joint_2_angle": np.degrees(joint_2_angle),
-            "joint_3_angle": np.degrees(joint_3_angle)}
+        ### calaculate first joint 2 angle
+
+        joint_2_angle = np.arctan(x/d) + np.arcsin(self.joint_lengths[2] * np.sin(joint_3_angle)/g)
+        # print(np.degrees(np.arcsin(leg.joint_lengths[2] * np.sin(joint_3_angle)/g)))
+
+        return {"joint_1_angle": np.degrees(joint_1_angle),
+                "joint_2_angle": np.degrees(joint_2_angle),
+                "joint_3_angle": np.degrees(joint_3_angle)}
 
 if __name__ == "__main__":
     LF_joint_lengths = [1, 2, 2]
@@ -57,5 +64,5 @@ if __name__ == "__main__":
     leg1 = Leg(LF_TYPE, LF_joint_lengths, LF_angles)
     targe = [1, 1, -2.8]
     base =  [0, 0, 0]
-    result = calculate_ik(leg1, target_position=targe, base_postion=base)
+    result = leg1.calculate_ik(target_position=targe, base_postion=base)
     print(result)
