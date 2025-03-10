e0d5526fb6381a867aba1d29c6bee9fc7bc8bc16
chenfukye <kye1234321@gmail.com>
2025-03-10 14:55:07 +0800

    [UPDATE] Update rl parameter

--- AI_pkg/gym_env/dual_leg_front_raise/rl_training_main.py
+++ AI_pkg/gym_env/dual_leg_front_raise/rl_training_main.py
@@ -36,8 +36,8 @@ class DualLegFrontRaiseDogEnv(gym.Env):
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete action
-        # self.action_space = Box(low=-10.0, high=10.0, shape=(1,), dtype=np.float32)
-        self.action_space = Discrete(11)
+        self.action_space = Box(low=-10.0, high=10.0, shape=(1,), dtype=np.float32)
+        # self.action_space = Discrete(7)
 
         # Example for using image as input:
         self.observation_space = Discrete(169)
@@ -46,12 +46,14 @@ class DualLegFrontRaiseDogEnv(gym.Env):
         self.total_eisode_reward = 0
         self.__pre_observation = None
         
-        self.motor_init_states = [0.0, 135.0, 90.0, 0.0, 135.0, 90.0,
-                                  0.0, 135.0, 90.0, 0.0, 135.0, 90.0]
+        self.motor_init_states = KeyboardAction.FORWARD_STEP_1[0]
+
         self.first_motor_angle = 0.0
-        self.step_timestep  = 0.03
-        self.targer_step = KeyboardAction.FORWARD_STEP_1.__len__()
+        self.step_timestep  = 0.02
+        self.targer_step = 30
         self.__start_time = time.time()
+
+        self.init_spot_yaw = 0.0
 
     def step(self, action):
         """
@@ -65,14 +67,14 @@ class DualLegFrontRaiseDogEnv(gym.Env):
         terminal = False
         done = False
 
-        direction = action - 5
-        scalar = 1.8
-        self.first_motor_angle = scalar * direction
+        # direction = action - 3
+        # scalar = 1.8
+        # self.first_motor_angle = scalar * direction
         action = float(action)
         
-        new_moter_states = KeyboardAction.FORWARD_STEP_1[self.cnt]
-        new_moter_states[3] = self.first_motor_angle
-        new_moter_states[9] = -self.first_motor_angle
+        new_moter_states = KeyboardAction.FORWARD_STEP_1[min(self.cnt, KeyboardAction.FORWARD_STEP_1.__len__() - 1)]
+        new_moter_states[3] = action
+        new_moter_states[9] = -action
 
         elisped_time = time.time() - self.__start_time
         if elisped_time < self.step_timestep:
@@ -100,13 +102,17 @@ class DualLegFrontRaiseDogEnv(gym.Env):
             angle_x_raw = observation["spot_angle"][0]
             angle_x =  angle_x_raw if angle_x_raw < 180 else 360 - angle_x_raw
 
+            angle_y_raw = observation["spot_angle"][1]
+            angle_y = angle_y_raw if angle_y_raw < 180 else 360 - angle_y_raw
+
             angle_z_raw = observation["spot_angle"][2]
             angle_z = angle_z_raw if angle_z_raw < 180 else 360 - angle_z_raw
 
             angle = np.sqrt(angle_x**2 + angle_z**2)
-            # print("step", self.cnt, "angle_x: ", angle_x, "angle_z: ", angle_z, "angle: ", angle)
-
-            if (angle > 15) and self.cnt > 0:
+            # print("step", self.cnt, "angle_x: ", angle_x, "angle_y: ", angle_y, "angle_z: ", angle_z, "angle: ", angle)
+            # print("init_spot_yaw: ", self.init_spot_yaw)
+            if (angle > 5 or abs(angle_y - self.init_spot_yaw) > 5) and self.cnt > 0:
+                print(angle_y)
                 terminal = True
                 done = True
                 reward -= 500 * (1 - (self.cnt / self.targer_step))
@@ -114,7 +120,7 @@ class DualLegFrontRaiseDogEnv(gym.Env):
         self.total_eisode_reward += reward
         if done:
             print("step: ", self.cnt, " total_eisode_reward: ", self.total_eisode_reward,
-                  " motor_angle: ", self.first_motor_angle)
+                  " motor_angle: ", action)
             log_episode_results(self.total_eisode_reward, self.cnt)
             self.cnt = 0
             self.total_eisode_reward = 0
@@ -132,11 +138,13 @@ class DualLegFrontRaiseDogEnv(gym.Env):
 
         # Reset the environment
         self.make_unity_env_reset()
-        time.sleep(1)
+        time.sleep(2)
 
         # Get observation
         observation, state = observation_cal.get_observation(self.__node)
         self.__pre_observation = observation
+        self.init_spot_yaw = observation["spot_angle"][1]
+        print("init_spot_yaw: ", self.init_spot_yaw)
         self.first_motor_angle = 0.0
         self.__start_time = time.time()
 
