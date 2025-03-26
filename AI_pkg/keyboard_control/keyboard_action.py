d17b3e72ca41922767df748c831c5e4ed00629ec
chenfukye <kye1234321@gmail.com>
2025-03-26 11:42:39 +0800

    [FEAT] Add new step for tort

--- AI_pkg/keyboard_control/keyboard_action.py
+++ AI_pkg/keyboard_control/keyboard_action.py
@@ -11,18 +11,22 @@ class KeyboardAction:
     """
     filename = "keyboard_control/keyboard_actions/motor_data_2025-03-21.pkl"
     filename_ik_craw = "keyboard_control/keyboard_actions/ik_craw_2025-03-25.pkl"
+    filename_ik_trot = "keyboard_control/keyboard_actions/ik_tort_2025-03-26.pkl"
+    
     with open(filename, "rb") as f:
         loaded_data = pickle.load(f)
     with open(filename_ik_craw, "rb") as f:
         loaded_data_ik_craw = pickle.load(f)
+    with open(filename_ik_trot, "rb") as f:
+        loaded_data_ik_trot = pickle.load(f)
     
     FORWARD_STEP_1 = loaded_data["FORWARD_STEP_1"]
 
-    FORWARD_STEP_2 = loaded_data_ik_craw["FORWARD_STEP_2"]
-    FORWARD_STEP_3 = loaded_data_ik_craw["FORWARD_STEP_3"]
+    FORWARD_STEP_2 = loaded_data_ik_trot["FORWARD_STEP_2"]
+    FORWARD_STEP_3 = loaded_data_ik_trot["FORWARD_STEP_3"]
     
-    FORWARD_STEP_4 = loaded_data_ik_craw["FORWARD_STEP_4"]
-    FORWARD_STEP_5 = loaded_data_ik_craw["FORWARD_STEP_5"]
+    FORWARD_STEP_4 = loaded_data_ik_trot["FORWARD_STEP_4"]
+    FORWARD_STEP_5 = loaded_data_ik_trot["FORWARD_STEP_5"]
     FORWARD_STEP_6 = loaded_data["SPOT_DANCE_6"]
     
     # make dog height from 0.15 to 0.20
