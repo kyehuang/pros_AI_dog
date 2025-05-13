6ce84b40df178aadf8fb7179a07634f6a33bbca1
chenfu mac <kye1234321@gmail.com>
2025-05-13 18:36:48 +0800

    [UPDATE] update the docker image in run docker shell

--- run_docker_pc.sh
+++ run_docker_pc.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 docker run -it --rm\
-    --gpus all\
     -p 9090:9090\
     -v $(pwd)/AI_pkg:/workspaces/AI_pkg\
-    registry.screamtrumpet.csie.ncku.edu.tw/kyehuang/pros_ppo_env:0.0.2\
+    registry.screamtrumpet.csie.ncku.edu.tw/kyehuang/pros_ppo_env:0.0.4\
     /bin/bash
