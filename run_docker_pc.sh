61d1cb7a6f2fa6d626afdbc5700807fc4079386c
chenfu mac <kye1234321@gmail.com>
2025-05-29 14:29:07 +0800

    [UPDATE] change docker image version

--- run_docker_pc.sh
+++ run_docker_pc.sh
@@ -3,5 +3,5 @@
 docker run -it --rm\
     -p 9090:9090\
     -v $(pwd)/AI_pkg:/workspaces/AI_pkg\
-    registry.screamtrumpet.csie.ncku.edu.tw/kyehuang/pros_ppo_env:0.0.4\
+    registry.screamtrumpet.csie.ncku.edu.tw/kyehuang/pros_ppo_env:0.0.5\
     /bin/bash
